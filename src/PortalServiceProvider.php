a16374d2bc7edc4834dfb9253cc41c3adb6b7cf0
Mahmoud Osman <mahmoud@winlocal.com>
2021-08-02 13:52:04 +0200

    migration

--- src/PortalServiceProvider.php
+++ src/PortalServiceProvider.php
@@ -8,69 +8,76 @@ use Illuminate\Support\ServiceProvider;
 
 class PortalServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'portal');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'portal');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/portal.php' => config_path('portal.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/portal'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/portal'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/portal'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
-        }
+	/**
+	 * Bootstrap the application services.
+	 */
+	public
+	function boot ()
+	{
+		/*
+		 * Optional methods to load your package assets
+		 */ // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'portal');
+		// $this->loadViewsFrom(__DIR__.'/../resources/views', 'portal');
+		 $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+		// $this->loadRoutesFrom(__DIR__.'/routes.php');
+		
+		if ( $this->app->runningInConsole() )
+		{
+			$this->publishes( [
+				                  __DIR__ . '/../config/portal.php' => config_path( 'portal.php' ),
+			                  ], 'portal-config' );
+			
+//			$this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+			
+			$this->publishes([
+				                 __DIR__.'/../database/migrations' => database_path('migrations'),
+			                 ], 'portal-migrations');
+			
+			// Publishing the views.
+			/*$this->publishes([
+				__DIR__.'/../resources/views' => resource_path('views/vendor/portal'),
+			], 'views');*/
+			
+			// Publishing assets.
+			/*$this->publishes([
+				__DIR__.'/../resources/assets' => public_path('vendor/portal'),
+			], 'assets');*/
+			
+			// Publishing the translation files.
+			/*$this->publishes([
+				__DIR__.'/../resources/lang' => resource_path('lang/vendor/portal'),
+			], 'lang');*/
+			
+			// Registering package commands.
+			// $this->commands([]);
+		}
+		
+		$this->configureGuard();
+	}
 	
-	    $this->configureGuard();
-    }
-
-    /**
-     * Register the application services.
-     */
-    public function register()
-    {
-	    config( [
-		            'auth.guards.portal' => array_merge( [
-			                                                 'driver'   => 'portal',
-			                                                 'provider' => null,
-		                                                 ], config( 'auth.guards.portal', [] ) ),
-	            ] );
-	
-	    if ( !app()->configurationIsCached() )
-	    {
-		    $this->mergeConfigFrom( __DIR__ . '/../config/portal.php', 'portal' );
-	    }
-
-        // Register the main class to use with the facade
-        $this->app->singleton('portal', function () {
-            return new Portal;
-        });
-    }
-	
+	/**
+	 * Register the application services.
+	 */
+	public
+	function register ()
+	{
+		config( [
+			        'auth.guards.portal' => array_merge( [
+				                                             'driver'   => 'portal',
+				                                             'provider' => null,
+			                                             ], config( 'auth.guards.portal', [] ) ),
+		        ] );
+		
+		if ( !app()->configurationIsCached() )
+		{
+			$this->mergeConfigFrom( __DIR__ . '/../config/portal.php', 'portal' );
+		}
+		
+		// Register the main class to use with the facade
+		$this->app->singleton( 'portal', function () {
+			return new Portal;
+		} );
+	}
 	
 	protected
 	function configureGuard ()
@@ -86,6 +93,7 @@ class PortalServiceProvider extends ServiceProvider
 	
 	protected
 	function createGuard ( $auth, $config )
+	: RequestGuard
 	{
 		return new RequestGuard( new Guard( $auth, config( 'portal.expiration' ), $config[ 'provider' ] ), request(),
 		                         $auth->createUserProvider( $config[ 'provider' ] ?? null ) );
